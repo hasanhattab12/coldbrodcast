571de49b8e525725c77567dc2fb1e5463e83f70c
hasanhattab12 <45760305+hasanhattab12@users.noreply.github.com>
2019-01-16 15:28:03 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "c";
+var adminprefix = '#'
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "obc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+
+}
+});
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
